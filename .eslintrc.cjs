6989c9f2f75f9b8bd360d3e59ca783bac4a5511c
OhYongUn <dhdyddjs@naver.com>
2024-02-10 12:43:57 +0900

    first commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  settings: { react: { version: '18.2' } },
+  rules: {
+    'react/jsx-no-target-blank': 'off',
+    // 여기에 필요한 다른 ESLint 규칙을 추가할 수 있습니다.
+  },
+}
